d02129da6752d4b3efc0ee6acff51e72ca56411a
Thanayut Seethongchuen <thanayut.see@outlook.com>
2021-07-23 15:53:30 +0700

    remove unused files

--- generate_bash_mybash.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo "BASH_DIR=$DIR/bash" > $DIR/.bash_mybash
-echo 'for f in $(ls $BASH_DIR/*.sh); do source $f; done' >> $DIR/.bash_mybash
-echo 'for f in $(ls $BASH_DIR/generated/*.sh); do source $f; done' >> $DIR/.bash_mybash
-echo 'for f in $(ls $BASH_DIR/generated/*.bash); do source $f; done' >> $DIR/.bash_mybash
-ln -sf $DIR/.bash_mybash ~/.bash_mybash
