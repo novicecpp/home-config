84396c22b0038829057ad2d52b91f900c0d943f1
Thanayut Seethongchuen <thanayut.see@outlook.com>
2022-06-12 18:27:36 +0200

    open file in server with tramp

--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 tmux_custom_split_windows () {
     x=$(tmux list-panes -F '#{pane_active} #{pane_pid} #{pane_current_command}' | grep -i '^1' | grep -v bash)
+    >2 echo $x
     if [[ "$1" == "horizontal" ]]; then
 	    tmux split-window -h -c "#{pane_current_path}"
     else
@@ -16,4 +17,27 @@ tmux_custom_split_windows () {
 
 tmux_copy_buffer_to_clipboard() {
     tmux show-buffer | xsel -ib
+}
+
+tmux_open_remote_file() {
+    if [[ $1 == "" ]]; then
+        DEBUG_TARGET=""
+        DEBUG_GREP_PID="^1"
+    else
+        DEBUG_TARGET="-t $1"
+        DEBUG_GREP_PID="0 $1"
+    fi
+    GREP_STRING='^##emacslocal'
+    CAPTURED=$(tmux capture-pane -p ${DEBUG_TARGET} | grep -- "${GREP_STRING}" | tail -n1)
+    if [[ "$CAPTURED" == "" ]]; then
+        return
+    fi
+    PANE_PID=$(tmux list-panes -F '#{pane_active} #{pane_index} #{pane_pid}' | grep "$DEBUG_GREP_PID" | cut -d' ' -f3)
+    PROCESS_PID=$(ps -o ppid= -o pid= -A | awk "\$1 == ${PANE_PID}{print \$2}")
+    IFS=' ' read P_COMMAND P_ARGS <<< $(ps --pid ${PROCESS_PID} -o args=)
+    if [[ $P_COMMAND != "ssh" ]]; then
+        return
+    fi
+    FILEPATH=$(echo ${CAPTURED} | awk '{print $2}')
+    emacsclient -cn "/ssh:${P_ARGS}:${FILEPATH}"
 }
