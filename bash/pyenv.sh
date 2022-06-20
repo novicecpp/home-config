c1e292530b571c62124c3e83202bd5841e758a02
Thanayut Seethongchuen <thanayut.see@outlook.com>
2022-06-20 12:05:28 +0200

    update pyenv init source

--- bash/pyenv.sh
+++ bash/pyenv.sh
@@ -1,22 +1,26 @@
-export PYENV_ROOT="$HOME/.local/share/pyenv"
-if [ -d "$PYENV_ROOT" ]; then
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    # from `pyenv init -`
-    export PATH="$PYENV_ROOT/shims:${PATH}"
-    export PYENV_SHELL=bash
-    source "$PYENV_ROOT/libexec/../completions/pyenv.bash"
-    pyenv() {
-      local command
-      command="${1:-}"
-      if [ "$#" -gt 0 ]; then
-        shift
-      fi
+#! /bin/bash
 
-      case "$command" in
-      activate|deactivate|rehash|shell)
-        eval "$(pyenv "sh-$command" "$@")";;
-      *)
-        command pyenv "$command" "$@";;
-      esac
-    }
-fi
+# copy from `pyenv init -` command
+
+PATH="$(bash --norc -ec 'IFS=:; paths=($PATH); for i in ${!paths[@]}; do if [[ ${paths[i]} == "'/home/thanayut/.local/share/pyenv/shims'" ]]; then unset '\''paths[i]'\''; fi; done; echo "${paths[*]}"')"
+export PATH="/home/thanayut/.local/share/pyenv/shims:${PATH}"
+export PYENV_SHELL=bash
+source '/home/thanayut/.local/share/pyenv/libexec/../completions/pyenv.bash'
+# disable rehash, it too slow
+# command pyenv rehash 2>/dev/null
+pyenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")"
+    ;;
+  *)
+    command pyenv "$command" "$@"
+    ;;
+  esac
+}
