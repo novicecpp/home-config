05112c848f82eb693ba88794658a8ef350292e41
Thanayut Seethongchuen <thanayut.see@outlook.com>
2023-04-04 01:57:50 +0200

    add cdh and replace cd with cdh

--- bash/function.sh
+++ bash/function.sh
@@ -89,7 +89,7 @@ f_sed_remove_comment_lines() {
 
 cdg()
 {
-    cd $(git rev-parse --show-toplevel)
+    cdh $(git rev-parse --show-toplevel)
 }
 
 
@@ -111,7 +111,7 @@ cdd() {
             break
         fi
     done
-    builtin cd "${INPUT_DIR}" &> /dev/null
+    cdh "${INPUT_DIR}" &> /dev/null
 }
 
 f_source_env () {
@@ -164,4 +164,58 @@ f_pyenv_init() {
     export PYENV_ROOT="$HOME/.pyenv"
     command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
+}
+
+
+# Copy from https://superuser.com/questions/299694/is-there-a-directory-history-for-bash/1395350#1395350
+cdh ()
+{
+    local hnum=16;
+    local new_dir index dir cnt;
+    if ! [[ $# -eq 0 ]]; then
+        # checking for "cd -- dir"
+        # but why? some practice?
+        if [[ $# -eq 2 && $1 = "--" ]]; then
+            shift;
+        else
+            # use builtin cd if passing cd option like
+            # "cd -L", "cd -L dir", but not this function's feature
+            # like "cd -12", "cd dir", "cd --".
+            if ! {
+                [[ $# -eq 1 ]] && [[ $1 =~ ^(-[0-9]{,2}|-|--|[^-].*)$ ]]
+            }; then
+                builtin cd "$@";
+                return;
+            fi;
+        fi;
+    fi;
+    [[ "$1" = "--" ]] && {
+        dirs -v;
+        return
+    };
+    new_dir=${1:-$HOME};
+    if [[ "$new_dir" =~ ^-[0-9]{,2}$ ]]; then
+        index=${new_dir:1};
+        # check for "cd -"
+        if [[ -z "$index" ]]; then
+            new_dir=$OLDPWD;
+        else
+            new_dir=$(dirs -l +$index) || return;
+        fi;
+    fi;
+    # NOTE: pushd always change dir
+    pushd -- "$new_dir" > /dev/null || return;
+    popd -n +$hnum &> /dev/null || true;
+    # remove duplicate history
+    new_dir=$PWD cnt=1;
+    while dir=$(dirs -l +$cnt 2> /dev/null); do
+        if [[ "$dir" = "$new_dir" ]]; then
+            popd -n +$cnt > /dev/null;
+            continue;
+        fi;
+        let cnt++;
+    done
+}
+cd() {
+    cdh "$@"
 }
