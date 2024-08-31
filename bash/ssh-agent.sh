97aabccb67a20b57efe2ed2072697a2004f3ad3c
Thanayut Seethongchuen <thanayut.see@outlook.com>
2024-08-31 20:22:42 +0200

    fix ssh-agent

--- bash/ssh-agent.sh
+++ bash/ssh-agent.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-SOCKDIR=$(ls /tmp | grep -E 'ssh-[A-Za-z0-9]{12}' | head -1)
-if [[ -z ${SOCKDIR} ]]; then
+SOCKPATH=$(find /tmp/ssh-* -name 'agent.*' -type s | head -1)
+if [[ -z ${SOCKPATH} ]]; then
     eval "$(ssh-agent)"
 else
-    SOCKPATH=$(find /tmp/"${SOCKDIR}" -type s)
     export SSH_AUTH_SOCK="${SOCKPATH}"
-    export SSH_AGENT_PID=$(($(cut -d'.' -f2 <<< $sockpath) + 1))
+    tmp=(${SOCKPATH/./ })
+    agentpid=${tmp[1]}
+    export SSH_AGENT_PID=$agentpid
 fi
