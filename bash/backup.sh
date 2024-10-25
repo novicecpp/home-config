ec7bd3b3d9834988950904f7e446c75bea979212
Thanayut Seethongchuen <thanayut.see@outlook.com>
2024-10-25 19:44:16 +0200

    local var and proper escape space

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 
 f_backup() {
+    local SRCDIR DSTDIR MACHINE_NAME DIR_NAME DIRSIZE
     if [[ $# != 2 ]]; then
        echo "Usage: "
        echo "bash backup.sh <src> <dst>"
        return 1
     fi
 
-    SRCDIR=$1
-    DSTDIR=$2
+    SRCDIR="${1}"
+    DSTDIR="${2}"
     MACHINE_NAME=${MACHINE_NAME:-any}
-    DIR_NAME=${DIR_NAME:-$(basename "$1")}
+    DIR_NAME="${DIR_NAME:-$(basename "$1")}"
     if [[ ! -d "${DSTDIR}" ]]; then
         >&2 echo "Error: backup path \"${DSTDIR}\" is not directory."
         return 1
     fi
 
-    printf -v BACKUP_PATH '%s/%(%Y-%m-%d)T_%s_%s.tar.zstd' "${DSTDIR%/}" -1 "$MACHINE_NAME" "$DIR_NAME"
+    printf -v BACKUP_PATH '%s/%(%Y-%m-%d)T_%s_%s.tar.zstd' "${DSTDIR%/}" -1 "${MACHINE_NAME}" "${DIR_NAME}"
 
-    echo "Backup from $SRCDIR to $BACKUP_PATH"
+    echo "Backup from $SRCDIR to ${BACKUP_PATH}"
     echo "Getting directory size: "
     DIRSIZE=$(du -sb "${SRCDIR}" | awk '{print $1}' | xargs)
     echo "$DIRSIZE bytes"
