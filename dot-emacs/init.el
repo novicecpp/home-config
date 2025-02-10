a8391f28dc7a1b61e1fd867202487e3ed0bfe9a5
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-02-10 07:20:49 +0100

    add grip-mode

--- dot-emacs/init.el
+++ dot-emacs/init.el
@@ -610,6 +610,17 @@
 (use-package terraform-mode
   :ensure t)
 
+;; markdown live
+(use-package ox-gfm
+  :ensure t)
+(use-package grip-mode
+  :ensure t
+  :init
+  (setq grip-use-mdopen t ;; to use `mdopen` instead of `grip`
+        grip-binary-path "~/.cargo/bin/mdopen")
+  (with-eval-after-load 'org
+    (bind-key "C-c C-g" #'grip-mode org-mode-map)))
+
 ;; load all file in dirs
 (mapc 'load (directory-files-recursively "~/.emacs.d/loads" ".el$"))
 (mapc 'load (directory-files-recursively "~/.emacs.d/private" ".el$"))
@@ -688,15 +699,6 @@
 
 ;;(use-package pdf-tools
 ;;  )
-
-;;(use-package impatient-mode
-;;  )
-;;
-;;(defun markdown-html (buffer)
-;;  (princ (with-current-buffer buffer
-;;    (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
-;;  (current-buffer)))
-
 
 ;; (add-to-list 'org-export-backends 'md)
 
