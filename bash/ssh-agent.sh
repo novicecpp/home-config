559d788c237ae2f2d896bb428879f7c6d4d60bee
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-03-18 09:26:47 +0100

    fix ssh-agent

--- bash/ssh-agent.sh
+++ bash/ssh-agent.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
-
-SOCKPATH=$(find /tmp/ssh-* -name 'agent.*' -type s | head -1)
-if [[ -z ${SOCKPATH} ]]; then
-    eval "$(ssh-agent)"
-else
+pidfile=/run/user/1000/ssh-agent.pid
+if [[ -f ${pidfile} ]]; then
+    pid=$(cat /run/user/1000/ssh-agent.pid)
+fi
+if [[ ${pid-x} != "x" ]] && ps -p "$pid" > /dev/null; then
+    SOCKPATH=$(find /tmp/ssh-* -name 'agent.*' -type s | grep "$((pid-1))")
     export SSH_AUTH_SOCK="${SOCKPATH}"
-    tmp=(${SOCKPATH/./ })
-    agentpid=${tmp[1]}
-    export SSH_AGENT_PID=$agentpid
+    export SSH_AGENT_PID=$pid
+else
+    eval "$(ssh-agent)"
+    echo "$SSH_AGENT_PID" > ${pidfile}
+    chmod 600 ${pidfile}
 fi
