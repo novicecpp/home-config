1869ff10a13f53a598f4945d9ee369fb7e560e7e
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-06-16 13:37:11 +0200

    better ux

--- bash/openssl.sh
+++ bash/openssl.sh
@@ -1,7 +1,7 @@
 f_openssl_web() {
     local endpoint host port
-    host=${1}
-    port=${2:-443}
-    endpoint=${host}:${port}
-    echo | openssl s_client -connect ${endpoint}  2>/dev/null | openssl x509 -noout -text
+    endpoint=${1}
+    host="${endpoint%:*}"
+    port="${endpoint#*:}"
+    echo | openssl s_client -connect ${endpoint} -servername ${host} 2>/dev/null | openssl x509 -noout -text
 }
