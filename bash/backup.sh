33c01a544552f1fed83c921ed440b42a402e05d1
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-06-21 21:38:38 +0200

    update f_backup to use b3sum and auto host discovery

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,35 +1,51 @@
 #! /bin/bash
 
 f_backup() {
-    local SRCDIR DSTDIR MACHINE_NAME DIR_NAME DIRSIZE
+    local SRCDIR DSTDIR machine_name backup_path dir_name dirsize filename chksum_algo chksum_algo_ext hostname
+    chksum_algo=b3sum
+    chksum_algo_ext=b3
     if [[ $# != 2 ]]; then
        echo "Usage: "
        echo "bash backup.sh <src> <dst>"
        return 1
     fi
-
+    hostname="$(hostnamectl hostname --static)"
     SRCDIR="${1}"
     DSTDIR="${2}"
-    MACHINE_NAME=${MACHINE_NAME:-any}
-    DIR_NAME="${DIR_NAME:-$(basename "$1")}"
+    DRY_RUN="${DRY_RUN-x}"
+    machine_name=${machine_name:-${hostname}}
+    dir_name="${dir_name:-$(basename "${SRCDIR}")}"
     if [[ ! -d "${DSTDIR}" ]]; then
         >&2 echo "Error: backup path \"${DSTDIR}\" is not directory."
         return 1
     fi
 
-    printf -v BACKUP_PATH '%s/%(%Y-%m-%d)T_%s_%s.tar.zstd' "${DSTDIR%/}" -1 "${MACHINE_NAME}" "${DIR_NAME}"
+    printf -v filename '%(%Y-%m-%d)T_%s_%s.tar.zst' -1 "${machine_name}" "${dir_name}"
+    backup_path="${DSTDIR%/}/${filename}"
 
-    echo "Backup from $SRCDIR to ${BACKUP_PATH}"
+    echo "Backup from $SRCDIR to ${backup_path}"
+    if [[ -f "${backup_path}" && "${DRY_RUN}" == "x" ]]; then
+        echo "Backup file exist: ${backup_path}"
+        read -r -p "Override? (Y/n): " choice
+        if [[ "$choice" != [Y] ]]; then
+            echo "Exit..."
+            return
+        fi
+    fi
     echo "Getting directory size: "
-    DIRSIZE=$(du -sb "${SRCDIR}" | awk '{print $1}' | xargs)
-    echo "$DIRSIZE bytes"
+    dirsize=$(du -sb "${SRCDIR}" | awk '{print $1}' | xargs)
+    echo "${dirsize} bytes"
 
-    if [[ -n $DRY_RUN ]]; then
+    if [[ ${DRY_RUN} != "x" ]]; then
        echo "This following command will get execute:"
-       echo "tar --ignore-failed-read -cf - \"${SRCDIR}\" | pv -brtp -s \"${DIRSIZE}\" | zstd -T0 - > \"${BACKUP_PATH}\""
+       echo "tar --ignore-failed-read -cf - \"${SRCDIR}\" | pv -brtp -s \"${dirsize}\" | zstd -T0 - > \"${backup_path}\""
        return
     fi
-    tar --ignore-failed-read -cf - "${SRCDIR}" | pv -brtp -s "${DIRSIZE}" | zstd -T0 - > "${BACKUP_PATH}"
+    tar --ignore-failed-read -cf - "${SRCDIR}" | pv -brtp -s "${dirsize}" | zstd -T0 - > "${backup_path}"
     echo "Running checksum..."
-    sha256sum "${BACKUP_PATH}" | tee "${BACKUP_PATH}.sha256"
+    (
+        pushd "${DSTDIR%/}" > /dev/null || return
+        ${chksum_algo} "${filename}" | tee "${filename}"."${chksum_algo_ext}"
+        popd > /dev/null || return
+    )
 }
