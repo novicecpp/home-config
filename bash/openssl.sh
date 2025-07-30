2589f6f7b7ff34faf7f2412e97b18d5ac9cd38fd
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-07-30 10:51:15 +0200

    openssl inspect

--- bash/openssl.sh
+++ bash/openssl.sh
@@ -5,3 +5,12 @@ f_openssl_web() {
     port="${endpoint#*:}"
     echo | openssl s_client -connect ${endpoint} -servername ${host} 2>/dev/null | openssl x509 -noout -text
 }
+
+f_openssl_inspect() {
+    if [[ -p /dev/stdin ]]; then
+       file=/dev/stdin
+    else
+       file=$1
+    fi
+    openssl x509 -in "${file}" -noout -text
+}
