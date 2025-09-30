3fc434a68fc0b5c5ccd7f657105255d70de0b836
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-09-30 19:22:44 +0200

    better ssl inspection command

--- bash/openssl.sh
+++ bash/openssl.sh
@@ -1,16 +1,35 @@
-f_openssl_web() {
-    local endpoint host port
-    endpoint=${1}
-    host="${endpoint%:*}"
-    port="${endpoint#*:}"
-    echo | openssl s_client -connect ${endpoint} -servername ${host} 2>/dev/null | openssl x509 -noout -text
-}
-
+# shellcheck disable=SC2119,SC2120
 f_openssl_inspect() {
+    local file
     if [[ -p /dev/stdin ]]; then
        file=/dev/stdin
     else
-       file=$1
+       file=${1}
     fi
-    openssl x509 -in "${file}" -noout -text
+    openssl crl2pkcs7 -nocrl -certfile "$file" | openssl pkcs7 -print_certs -text -noout
+}
+
+f_openssl_s_client () {
+    local endpoint host port pattern
+    endpoint=${1}
+    host="${endpoint%:*}"
+    pattern='^[a-z.]+\:[0-9]+$'
+    if [[ ${endpoint} =~ $pattern ]]; then
+        port="${endpoint#*:}"
+    else
+        port=443
+    fi
+    echo | openssl s_client -connect ${host}:${port} -servername ${host}
+}
+
+f_openssl_inspect_web() {
+    local endpoint
+    endpoint=${1}
+    f_openssl_s_client ${endpoint} | awk '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/' | f_openssl_inspect
+}
+
+
+f_openssl_inspect_k8s_secret() {
+    secretname=${1}
+    kubectl get secret -o jsonpath='{.data.tls\.crt}' ${secretname} | base64 -d | f_openssl_inspect
 }
