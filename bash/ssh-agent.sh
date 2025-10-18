84b4495d01a1d76120d343e54eea35f980b00d75
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-10-18 22:00:58 +0200

    comment out for now

--- bash/ssh-agent.sh
+++ bash/ssh-agent.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-pidfile=/run/user/1000/ssh-agent.pid
-sockpath=/run/user/1000/ssh-agent.sock
-if [[ -f ${pidfile} ]]; then
-    pid=$(cat /run/user/1000/ssh-agent.pid)
-fi
-if [[ ${pid-x} != "x" ]] && ps -p "$pid" > /dev/null; then
-    export SSH_AUTH_SOCK="${SOCKPATH:-${sockpath}}"
-    export SSH_AGENT_PID=$pid
-else
-    eval "$(ssh-agent -a ${sockpath})"
-    echo "$SSH_AGENT_PID" > ${pidfile}
-    chmod 600 ${pidfile}
-fi
+#pidfile=/run/user/1000/ssh-agent.pid
+#sockpath=/run/user/1000/ssh-agent.sock
+#if [[ -f ${pidfile} ]]; then
+#    pid=$(cat /run/user/1000/ssh-agent.pid)
+#fi
+#if [[ ${pid-x} != "x" ]] && ps -p "$pid" > /dev/null; then
+#    export SSH_AUTH_SOCK="${SOCKPATH:-${sockpath}}"
+#    export SSH_AGENT_PID=$pid
+#else
+#    eval "$(ssh-agent -a ${sockpath})"
+#    echo "$SSH_AGENT_PID" > ${pidfile}
+#    chmod 600 ${pidfile}
+#fi
