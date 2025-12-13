0a68fdd53cbef593efcf2e4be46fea608648077e
Thanayut Seethongchuen <thanayut.see@outlook.com>
2025-12-13 23:30:56 +0100

    ignore mounted loopback

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 f_backup() {
-    local SRCDIR DSTDIR machine_name backup_path dir_name dirsize filename chksum_algo chksum_algo_ext hostname
+    local SRCDIR DSTDIR machine_name backup_path dir_name dirsize filename chksum_algo chksum_algo_ext hostname ignorepath_rel exclude_args p
     chksum_algo=b3sum
     chksum_algo_ext=b3
     if [[ $# != 2 ]]; then
@@ -11,6 +11,8 @@ f_backup() {
     fi
     hostname="$(hostnamectl hostname --static)"
     SRCDIR="${1}"
+    # sanitize path to match --exclude in tar, to add `./` in front and remove `/` in back
+    [[ "${SRCDIR}" != ./* && "${SRCDIR}" != /* ]] && SRCDIR="./${SRCDIR%/}"
     DSTDIR="${2}"
     DRY_RUN="${DRY_RUN-x}"
     machine_name=${MACHINE_NAME:-${hostname}}
@@ -40,13 +42,20 @@ f_backup() {
     dirsize=$(du -sb "${SRCDIR}" | awk '{print $1}' | xargs)
     dirhsize=$(numfmt --to=iec-i --suffix=B --format="%.3f" ${dirsize})
     echo "${dirsize} bytes, ${dirhsize}"
-
+    # ignore the encrypted loopback
+    declare -a exclude_args
+    while read -r p; do
+        ignorepath_rel="$(realpath -s --relative-to="${SRCDIR}" "${p}")"
+        exclude_args+=("--exclude ${SRCDIR}/${ignorepath_rel}")
+    done <<< "$(df -hT | grep -v /dev/mapper/root | grep /dev/mapper | awk '{print $7}')"
+    cmd="tar --ignore-failed-read ${exclude_args[*]} -cf - ${SRCDIR} | pv -brtp -s ${dirsize} | zstd -T0 - > ${backup_path}"
     if [[ ${DRY_RUN} != "x" ]]; then
        echo "This following command will get execute:"
-       echo "tar --ignore-failed-read -cf - \"${SRCDIR}\" | pv -brtp -s \"${dirsize}\" | zstd -T0 - > \"${backup_path}\""
+       echo "${cmd}"
        return
     fi
-    tar --ignore-failed-read -cf - "${SRCDIR}" | pv -brtp -s "${dirsize}" | zstd -T0 - > "${backup_path}"
+    eval "$(echo "${cmd}" | paste -s -d " " -)"
+    #tar --ignore-failed-read -cf - "${SRCDIR}" | pv -brtp -s "${dirsize}" | zstd -T0 - > "${backup_path}"
     echo "Running checksum..."
     (
         pushd "${DSTDIR%/}" > /dev/null || return
