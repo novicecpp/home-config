78885b9440b954eecf3f49b8eecbe3bb2d3f7aa5
Thanayut Seethongchuen <thanayut.see@outlook.com>
2026-05-06 21:26:59 +0200

    tmux: break pane new-session that also work with remote shell

--- bash/tmux.sh
+++ bash/tmux.sh
@@ -31,6 +31,7 @@ term () {
     fi
 }
 
+
 # open file in already open emacsclient in current session
 tmux_emacsclient () {
     if [[ -z $TMUX ]]; then
@@ -236,19 +237,85 @@ t_cp() {
 
 t_break_pane_new_session () {
     local session_name
+    set +x
     session_name=${1:-}
+    source_pane=${2:-}
     if [[ -z ${session_name} ]]; then
         >&2 echo "Usage: $FUNCNAME SESSION_NAME"
         return 1
     fi
     if ! tmux has-session -t ${session_name} &> /dev/null; then
-        tmux new-session -d -s ${session_name}
-        tmux new-window -t ${session_name}:1
-        tmux move-pane -t ${session_name}:1
-        tmux kill-pane -t ${session_name}:1.1
+        tmux new-session -d -s ${session_name} \; new-window -t ${session_name}:1 \; move-pane -s ${source_pane} -t ${session_name}:1 \; kill-pane -t ${session_name}:1.1
         tmux switch-client -t ${session_name}:1.1
     else
         >&2 echo "Error: tmux session already exists: \"${session_name}\""
         return 1
     fi
 }
+
+########### from old tmux.sh #########
+
+tmux_custom_split_windows () {
+    x=$(tmux list-panes -F '#{pane_active} #{pane_pid} #{pane_current_command}' | grep -i '^1' | grep -v bash)
+    >2 echo $x
+    if [[ "$1" == "horizontal" ]]; then
+	    tmux split-window -h -c "#{pane_current_path}"
+    else
+	    tmux split-window -c "#{pane_current_path}"
+    fi
+    if [[ -n "$x" ]]; then
+	    w=$(echo $x | awk -F ' ' '{print $2}')
+        y=$(ps -o ppid= -o pid= -A | awk "\$1 == $w{print \$2}")
+	    z=$(ps -p $y -o command=)
+	tmux send-key "$z"
+    fi
+}
+
+tmux_copy_buffer_to_clipboard() {
+    tmux show-buffer | wl-copy --type text/plain
+}
+
+tmux_open_remote_file() {
+    if [[ $1 == "" ]]; then
+        DEBUG_TARGET=""
+        #DEBUG_GREP_PID="^1"
+    else
+        DEBUG_TARGET="-t $1"
+        #DEBUG_GREP_PID="0 $1"
+    fi
+    GREP_STRING='^##emacslocal'
+    CAPTURED=$(tmux capture-pane -p -J ${DEBUG_TARGET} | grep -- "${GREP_STRING}" | tail -n1)
+    if [[ "$CAPTURED" == "" ]]; then
+        return
+    fi
+    #PANE_PID=$(tmux list-panes -F '#{pane_active} #{pane_index} #{pane_pid}' | grep "$DEBUG_GREP_PID" | cut -d' ' -f3)
+    #PROCESS_PID=$(ps -o ppid= -o pid= -A | awk "\$1 == ${PANE_PID}{print \$2}")
+    #IFS=' ' read P_COMMAND P_ARGS <<< $(ps --pid ${PROCESS_PID} -o args=)
+
+    #if [[ $P_COMMAND != "ssh" ]]; then
+    #    return
+    #fi
+    #FILEPATH=$(echo ${CAPTURED} | awk '{print $2}')
+    arglist=(${CAPTURED})
+    P_ARGS=${arglist[1]}
+    FILEPATH=${arglist[2]}
+    emacsclient -cn "/ssh:${P_ARGS}:${FILEPATH}"
+}
+
+tmux_copy_shell_output_to_clipboard() {
+    if [[ -z $DEBUG_PANE ]]; then
+        DEBUG_TARGET=""
+    else
+        DEBUG_TARGET="-t $DEBUG_PANE"
+    fi
+    CAPTURED=$(tmux capture-pane -p -S -1000 ${DEBUG_TARGET})
+    if [[ "$CAPTURED" == "" ]]; then
+        return
+    fi
+    readarray -t GREP_OUTPUT_ARRAY <<< $(printf "%s" "$CAPTURED" | grep -n -P '\[.+@.+ .+\]' | tail -n2)
+    FIRST=$(( ${GREP_OUTPUT_ARRAY[0]%%[:]*} + 1 ))
+    LAST=$(( ${GREP_OUTPUT_ARRAY[1]%%[:]*} - 1 ))
+    printf "%s\n" "$CAPTURED" | sed -n "${FIRST},${LAST}p" | xsel -ib
+}
+
+######################################
